dca066da448f0fb97d683b8b69fd6fe5ea5274f9
joylunow <zavoianugabriel70@icloud.com>
2024-07-13 14:54:25 +0300

    update 3

--- server/db/redis_client.go
+++ server/db/redis_client.go
@@ -1,19 +1,13 @@
 package db
 
 import (
-	"crypto/tls"
-
 	"github.com/redis/go-redis/v9"
 )
 
 func InitializeRedisClient() (*redis.Client, error) {
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "127.0.0.1:6379",
-		TLSConfig: &tls.Config{
-			MinVersion: tls.VersionTLS12,
-			ServerName: "joylunow.dev",
-		},
-		DB: 0,
+		Addr: "joylunow.dev:6379",
+		DB:   0,
 	})
 	return rdb, nil
 }
